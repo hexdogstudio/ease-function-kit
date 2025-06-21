abc2f411625f3295bf9a81f040068efe68b18afc
Hexdog <127632458+hexdogstudio@users.noreply.github.com>
2025-06-21 10:25:26 +0200

    Update EaseCurveDrawer.cs

--- Editor/EaseCurveDrawer.cs
+++ Editor/EaseCurveDrawer.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using UnityEditor;
 using UnityEngine;
 
@@ -19,3 +20,4 @@ public class EaseCurveDrawer : PropertyDrawer
         return property.FindPropertyRelative("m_Type");
     }
 }
+#endif
